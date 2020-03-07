e5bce6ae2fb0017d4a03b448c33a2ac9cf7406fb
Vitor Alves <Vitoralve81@gmail.com>
2020-03-07 12:37:38 -0300

    Primero commit para inicio

---
+++ app/src/main/java/com/example/janela/gestao/de/presenca/QrCode/MeuQrCode.java
@@ -0,0 +1,16 @@
+package com.example.janela.gestao.de.presenca.QrCode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.janela.gestao.de.presenca.R;
+
+public class MeuQrCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meu_qr_code);
+    }
+}
